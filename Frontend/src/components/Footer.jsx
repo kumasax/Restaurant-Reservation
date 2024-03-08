151ae9a4f211602d7281012c8701453d40d09430
kumasax <kumarsanskar12nov@gmail.com>
2024-03-08 16:30:05 +0530

    First commit

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">SANIII</div>
+          <div className="right">
+            <p>Sanskar Kumar</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Sanskar</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Sanskar.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
